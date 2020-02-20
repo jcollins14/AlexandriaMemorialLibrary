4c474396cb46bdfcb2c19ab3b01dadcd379cdb65
Jake Collins <jakecollins1014@gmail.com>
2020-02-19 21:51:07 -0500

    initial files

---
+++ AlexandriaMemorialLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlexandriaMemorialLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
