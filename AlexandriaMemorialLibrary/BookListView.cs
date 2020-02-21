232050147be1f2e849c6eb42c42e7a61c191717c
Jake Collins <jakecollins1014@gmail.com>
2020-02-20 21:01:51 -0500

    fleshed out SearchTitle()

--- AlexandriaMemorialLibrary/BookListView.cs
+++ AlexandriaMemorialLibrary/BookListView.cs
@@ -36,6 +36,7 @@ namespace AlexandriaMemorialLibrary
                     }
 
                 }
+                i++;
                 Console.WriteLine(listing);
             }
         }
--- AlexandriaMemorialLibrary/LibraryController.cs
+++ AlexandriaMemorialLibrary/LibraryController.cs
@@ -44,23 +44,88 @@ namespace AlexandriaMemorialLibrary
                 DueDate = new DateTime(1800,1,1)
             }
             );
+            Library.Add(new Book()
+            {
+                Title = "Radicalized",
+                Author = "Cory Doctorow",
+                ISBN = 9781250228598,
+                Status = Status.OnShelf,
+                //Genre = new List<Genre>() { Genre.SciFi, Genre.Adventure, Genre.Fantasy }
+                DueDate = new DateTime(1800, 1, 1)
+            }
+            );
+            Library.Add(new Book()
+            {
+                Title = "Every Tool's a Hammer",
+                Author = "Adam Savage",
+                ISBN = 9781471185113,
+                Status = Status.OnShelf,
+                //Genre = new List<Genre>() { Genre.SciFi, Genre.Adventure, Genre.Fantasy }
+                DueDate = new DateTime(1800, 1, 1)
+            }
+            );
+            Library.Add(new Book()
+            {
+                Title = "Mud, Sweat, and Tears",
+                Author = "Bear Grylls",
+                ISBN = 9780062124135,
+                Status = Status.OnShelf,
+                //Genre = new List<Genre>() { Genre.SciFi, Genre.Adventure, Genre.Fantasy }
+                DueDate = new DateTime(1800, 1, 1)
+            }
+            );
+            Library.Add(new Book()
+            {
+                Title = "World War Z",
+                Author = "Max Brooks",
+                ISBN = 9780307346612,
+                Status = Status.OnShelf,
+                //Genre = new List<Genre>() { Genre.SciFi, Genre.Adventure, Genre.Fantasy }
+                DueDate = new DateTime(1800, 1, 1)
+            }
+            );
         }
 
         public void Run()
         {
-            BookListView library = new BookListView(Library);
-            library.Display();
-            Console.WriteLine();
-            Book dune = Library[0];
-            dune.CheckOut();
-            Library[0] = dune;
-            BookListView test = new BookListView(Library);
-            test.Display();
+            List<Book> search = SearchTitle(Library);
+            BookListView list = new BookListView(search);
+            if (search.Count == 0)
+            {
+                Console.WriteLine("I'm sorry, there were no results found.");
+            }
+            else
+            {
+                list.Display();
+            }
+
         }
 
-        public void Search()
+        public List<Book> SearchTitle(List<Book> library)
         {
-            Console.WriteLine("To Be Implemented");
+            List<Book> searchResults = new List<Book>();
+
+            string search = Console.ReadLine().ToLower().Trim();
+
+            foreach (Book book in library)
+            {
+                string title = book.Title;
+                List<char> delimiter = new List<char>() { ' ', ',', '\'', '?', '.', '!', '"', ':', '-', '&' };
+                for (int i = 0; i < title.Length; i++)
+                {
+                    if (delimiter.Contains(title[i]))
+                    {
+                        title = title.Remove(i,1);
+                    }
+                }
+                title = title.ToLower();
+                if (title.Contains(search))
+                {
+                    searchResults.Add(book);
+                }
+            }
+
+            return searchResults;
         }
 
         public int UserInput()
